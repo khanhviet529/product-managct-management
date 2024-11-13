9957ef1c507d95007db853a90c8134816c6a4b7b
khánh nguyễn <khanhviet472003@gmail.com>
2024-11-14 00:02:51 +0700

    cap nhat

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const methodOverride = require('method-override')
-const flash = require('express-flash')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
+const express = require('express');
+const methodOverride = require('method-override');
+const flash = require('express-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 const routerClient = require('./routes/client/index.route');
 const routerAdmin = require('./routes/admin/index.route');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const database = require('./config/database');
 const systemConfig = require('./config/system');
 
 database.connect();
 
-const app = express()
-const port = process.env.PORT 
-app.set('view engine', 'pug')
-app.set('views', './views')
+const app = express();
+const port = process.env.PORT ;
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
 
 //overrider method
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded());
 
 //flash
 app.use(cookieParser('zxcvvvvbbbb'));
@@ -35,8 +35,8 @@ routerClient(app);
 routerAdmin(app);
   
 // Phía bên người dùng sẽ xem được những mục được public
-app.use(express.static('public'))
-
+app.use(express.static(`${__dirname}/public`));
+console.log('Server is running at http://localhost:' + __dirname)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
